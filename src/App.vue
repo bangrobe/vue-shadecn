68d077e8992501848f95f7e0bf99138ed82a6071
bangrobe <bangrobe@gmail.com>
2024-09-22 10:47:54 +0700

    Vue3, tailwind, shadecn, vue-router, pinia

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<template>
+  <header>
+    <Button>Click me</Button>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
