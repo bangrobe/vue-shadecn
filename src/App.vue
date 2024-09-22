960d29b3268008ac97612fb12d3c6ff5b2bbfbca
bangrobe <bangrobe@gmail.com>
2024-09-22 10:14:51 +0700

    Get started

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div>
+   <h1>Inventory Management</h1>
+   <Button>Click me</Button>
+  </div>
+</template>
+
